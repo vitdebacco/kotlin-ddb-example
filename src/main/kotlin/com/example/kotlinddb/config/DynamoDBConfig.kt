99cbcf05aee78d9fe54f7bee633494ce47e7bc6e
vitdebacco <vit.debacco@validic.com>
2021-01-09 15:33:01 -0500

    feat: configure DynamoDB using Jooby extension module (#3)
    This commit refactors DynamoDB client config leveraging Jooby's extension
    module mechanism to register the DynamoDB client as an application service.
    
    For more info, please see: https://jooby.io/#extensions-and-services

--- src/main/kotlin/com/example/kotlinddb/config/DynamoDBConfig.kt
+++ src/main/kotlin/com/example/kotlinddb/config/DynamoDBConfig.kt
@@ -2,30 +2,18 @@ package com.example.kotlinddb.config
 
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider
-// import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient
 import java.net.URI
 
 class DynamoDBConfig {
-
     // https://forums.aws.amazon.com/thread.jspa?messageID=717048 #RAGE
 
-    private val client: DynamoDbClient = DynamoDbClient.builder()
-        .endpointOverride(URI.create("http://localhost:8000"))
-//        .region(Region.of("ddblocal"))
-        .credentialsProvider(
-            StaticCredentialsProvider.create(AwsBasicCredentials.create("key", "secret"))
-        )
-        // AWSStaticCredentialsProvider(BasicAWSCredentials("key", "secret"))
-//        .credentialsProvider(DefaultCredentialsProvider.builder().build())
-        .build()
-
-//    fun enhancedClient(): DynamoDbEnhancedClient {
-//        return DynamoDbEnhancedClient.builder()
-//            .dynamoDbClient(client()).build()
-//    }
-
     fun client(): DynamoDbClient {
-        return client
+        return DynamoDbClient.builder()
+            .endpointOverride(URI.create("http://localhost:8000"))
+            .credentialsProvider(
+                StaticCredentialsProvider.create(AwsBasicCredentials.create("key", "secret"))
+            )
+            .build()
     }
 }
